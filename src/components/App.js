ef0ee3d3f2c6e00f7ac55318e07d898ae600d593
Maria SJ <maria.som.jur@gmail.com>
2022-11-28 08:47:47 +0100

    starter kit react and github pages

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import '../styles/App.scss';
+//import {useEffect, useState} from 'react';
+//import callToApi from '../services/api';
+//import ls from '../services/localStorage';
+
+// STATES
+
+// USEEFFECT
+
+// useEffect(() => {
+//   // Dentro de useEffect llamamos a la API
+//   callToApi().then((data) => {
+//     // Cuando la API responde guardamos los datos en el estado para que se vuelva a renderizar el componente X
+//     X(data);
+//   });
+//   // Aquí ponemos un array vacío porque solo queremos que se llame a la API la primera vez
+// }, []);
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
